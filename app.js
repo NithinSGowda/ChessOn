2039c024e5b262f6d9a3cf2128791006a529af7a
qaziamaan <qazi9amaan@gmail.com>
2021-04-12 15:54:29 +0530

    integrated the front end with the back end

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const path = require('path')
 const app = express()
 const port = 4000
 
+const jsChessEngine = require('js-chess-engine')
+const game = new jsChessEngine.Game()
+
+
+
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -12,6 +17,24 @@ app.get('/', function (req, res){
 
 app.get('/newgame', function (req, res){
     res.sendFile(__dirname+'/public/game.html');
+});
+
+
+app.get('/move', function(req, res){
+
+  try {
+    game.move(req.query.from,req.query.to,req.query.config)
+    result = game.aiMove(0,req.query.config)
+    res.json({
+      move : result,
+      board : game.board.configuration
+    });
+  } catch (error) {
+    res.send(`invalid-move;${req.query.to}-${req.query.from}`)
+
+  }
+  
+
 });
 
 app.listen(port, () => {
